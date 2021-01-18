7dc057bd1d6a4f98219f3937aa9deb6e83e904b3
geekaven <i@tawawa.moe>
2021-01-18 22:37:19 +0800

    feat: init springboot project
    #1

---
+++ src/test/java/moe/yuyuyu/ani/AnimeCApplicationTests.java
@@ -0,0 +1,13 @@
+package moe.yuyuyu.ani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeCApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
